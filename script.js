67c71d01c93c85372f306215a5557819a14d5122
Sajadmhy <sajad.mahyaei@gmail.com>
2022-03-16 08:28:49 -0400

    Add rainbow function to script.js

--- script.js
+++ script.js
@@ -20,11 +20,7 @@ createGrid(16);
 grids = document.getElementsByClassName("grid");
 
 // clearGrid sets all the div's colors to white
-function clearGrid() {
-   
-    for (let k = 0; k < grids.length; k++){
-            grids[k].style.backgroundColor = "white";
-        }};
+
 
 // trail set's any cell's color to black on mouse hover
 function trail () {
@@ -32,21 +28,44 @@ for (let k = 0; k < grids.length; k++){
     grids[k].addEventListener("mouseover", function() {
         grids[k].style.backgroundColor = "black";
     })}};
-    
+ 
+    function clearGrid() {
+   
+        for (let k = 0; k < grids.length; k++){
+                grids[k].style.backgroundColor = "white";
+            trail();
+            }};
+ 
 trail();
 
 //refreshGrid asks for number of cells, clears the grid
 // and creates new grid and activates the trail function
-function refreshGrid () {
+function refreshGrid () {    
+    
     let z = prompt(`How many boxes per side?`);
-    while (grids.length > 0) {
+   
+    if (z > 200) { return alert("Error")} 
+    else if (z <=200) { while (grids.length > 0) {
         var grid = grids[0];
         grid.parentNode.removeChild(grid);
     };
     createGrid(z);
     trail();
-};
+}};
+
+function rainbow () {
+    const randomColor = Math.floor(Math.random()*16777215).toString(16);
+    for (let p = 0; p< grids.length; p++){
+        let randomColor = Math.floor(Math.random()*16777215).toString(16);
+        grids[p].addEventListener("mouseover",function () {
+        grids[p].style.backgroundColor = "#" + randomColor;
+
+        })}};
+  
+
 
 document.getElementById("set-grid").addEventListener("click", refreshGrid);
 document.getElementById("clear").addEventListener("click", clearGrid);
-    
+document.getElementById("rainbow").addEventListener("click", rainbow);
+
+
